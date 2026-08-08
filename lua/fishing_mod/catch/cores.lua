4398640b696feefe269158f63b46ed816add05be
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-08-09 01:10:18 +0300

    More fixes

--- lua/fishing_mod/catch/cores.lua
+++ lua/fishing_mod/catch/cores.lua
@@ -19,101 +19,13 @@ local ENT = {}
 ENT.Type = "anim"
 ENT.Base = "fishing_mod_base"
 
-function ENT:SetupDataTables()
-	self:DTVar("Int", 0, "Core")
-end
-
 if SERVER then
 
 	function ENT:Initialize()
 
-		self:SetModel("models/props_bts/glados_ball_reference.mdl")
+		self:SetModel("models/hunter/misc/sphere075x075.mdl")
+		self:SetColor(Color(55, 0, 255))
 		self:PhysicsInit(SOLID_VPHYSICS)
-		self:SetMoveType(MOVETYPE_VPHYSICS)
-		self:SetSolid(SOLID_VPHYSICS)
-		self:SetCollisionGroup(COLLISION_GROUP_INTERACTIVE)
-
-		self.dt.Core = math.random(0,3)
-
-		self.body = ents.Create("prop_dynamic")
-		self.body:SetModel("models/props_bts/glados_ball_reference.mdl")
-		self.body:SetAngles(self:GetAngles())
-		self.body:SetPos(self:GetPos())
-		self.body:SetParent(self)
-		self.body:Spawn()
-
-		self.body:SetSkin(self.dt.Core)
-
-		if self.dt.Core == 0 then
-			self.Anim = self:LookupSequence("idle")
-		else
-			self.Anim = self:LookupSequence("Look_0"..self.dt.Core+1)
-		end
-
-	end
-
-	function ENT:Think()
-		if not IsValid(self.body) then return end
-		self.body:ResetSequence(self.Anim)
-	end
-
-	function ENT:OnRemove()
-
-		SafeRemoveEntity(self.body)
-
-	end
-
-else
-
-	ENT.Sounds = {
-		[1]={},
-		[2]={},
-		[3]={}
-	}
-
-	for i=1,17 do
-		if i<=9 then
-			ENT.Sounds[1][i] = "vo/aperture_ai/escape_02_sphere_curiosity-0"..i..".wav"
-		else
-			ENT.Sounds[1][i] = "vo/aperture_ai/escape_02_sphere_curiosity-"..i..".wav"
-		end
-	end
-
-	for i=0, 21 do
-		if i<=9 then
-			ENT.Sounds[2][i] = "vo/aperture_ai/escape_02_sphere_anger-0"..i..".wav"
-		else
-			ENT.Sounds[2][i] = "vo/aperture_ai/escape_02_sphere_anger-"..i..".wav"
-		end
-	end
-
-	for i=1,41 do
-		if i<=9 then
-			ENT.Sounds[3][i] = "vo/aperture_ai/escape_02_sphere_cakemix-0"..i..".wav"
-		else
-			ENT.Sounds[3][i] = "vo/aperture_ai/escape_02_sphere_cakemix-"..i..".wav"
-		end
-	end
-
-	function ENT:Initialize()
-
-		self.LastSound = 1
-
-	end
-
-	function ENT:Think()
-
-		if (self.LastSound <= CurTime()) and not(self.dt.Core == 0) then
-			local curSound = table.Random(self.Sounds[self.dt.Core])
-
-			self:EmitSound(curSound, 80, 100)
-
-			self.LastSound = CurTime()+SoundDuration(curSound)+0.1
-		end
-
-	end
-
-	function ENT:Draw()
 	end
 
 end
--- lua/fishing_mod/catch/heavy.lua
+++ lua/fishing_mod/catch/heavy.lua
@@ -4,9 +4,9 @@ if util.IsValidRagdoll("models/player/heavy.mdl") and heavy_enabled:GetBool() th
 		friendly = "Stoned Heavy",
 		type = "fishing_mod_catch_heavy",
 		size = 8,
-		rareness = 3500, 
-		yank = 100000, 
-		mindepth = 100, 
+		rareness = 3500,
+		yank = 100000,
+		mindepth = 100,
 		maxdepth = 20000,
 		expgain = 70,
 		value = 300,
@@ -40,7 +40,7 @@ if SERVER then
 		self:SetNoDraw(true)
 		self.Heavy = ents.Create("prop_ragdoll")
 		local heavy = self.Heavy
-		heavy:SetModel("models/player/heavy.mdl")
+		heavy:SetModel("models/stalker.mdl")
 		heavy:SetPos(self:GetPos())
 		heavy:Spawn()
 		self:SetParent(self.Heavy)
@@ -48,14 +48,14 @@ if SERVER then
 		self.dt.Heavy = heavy
 		self.health = max_health
 	end
-	
+
 	function ENT:PreHook(player)
 		if IsValid(player.FMHeavy) then
 			player:EmitSound("buttons/button8.wav")
 			return false
 		end
 	end
-	
+
 	function ENT:PostHook(player)
 		local number = math.random(17)
 		if number < 10 then
@@ -67,7 +67,7 @@ if SERVER then
 		player.FMHeavy = self.Heavy
 		if self.Heavy.CPPISetOwner then self.Heavy:CPPISetOwner(player) end
 	end
-	
+
 	function ENT:PlaySound(path)
 		if not self.busysound then
 			self.Heavy:EmitSound(path, 100, math.random(90,110))
@@ -77,12 +77,12 @@ if SERVER then
 			if not IsValid(self) then return end
 			self.busysound = false
 		end)
-	end	
-	
+	end
+
 	function ENT:EntityTakeDamage(entity, data)
 		if entity ~= self.Heavy then return end
 		local amount=data:GetDamage() > 50 and 50 or data:GetDamage()
-		
+
 		if self.health > 0 then
 			self:PlaySound("vo/heavy_painsharp0"..math.random(5)..".mp3")
 			self.health = self.health - amount
@@ -92,15 +92,15 @@ if SERVER then
 			local heavy = self.Heavy
 			for i=0,15 do
 				local phys = heavy:GetPhysicsObjectNum(i)
-				phys:EnableGravity(true)	
+				phys:EnableGravity(true)
 			end
 		end
-	end 
+	end
 
 	function ENT:Think()
 		self.Heavy:SetColor(self:GetColor())
-		
-		if self.dt.dead or self.health ~= max_health then 
+
+		if self.dt.dead or self.health ~= max_health then
 			for key, entity in pairs(ents.FindInSphere(self.Heavy:GetPos(), 50)) do
 				if entity:GetModel() and string.find(entity:GetModel():lower(), "medkit") then
 					entity:Remove()
@@ -109,20 +109,20 @@ if SERVER then
 					self:PlaySound("vo/heavy_positivevocalization0"..math.random(5)..".mp3")
 				end
 			end
-			
+
 			if math.random() > 0.9 then
 				self:PlaySound("vo/heavy_medic0"..math.random(3).."mp3")
 			end
-			return 
+			return
 		end
-		
+
 		for key, seagull in pairs(ents.FindByClass("fishing_mod_seagull")) do
 			local handpos = self.Heavy:GetBonePosition(self.Heavy:LookupBone("bip_hand_l"))
 			local distance = seagull:GetPos():Distance(handpos)
 			if distance < 4000 then
 				self.target = seagull
 			end
-			
+
 			if distance < 60 then
 				local data = DamageInfo()
 				data:SetAttacker(self.Heavy.owner)
@@ -139,62 +139,62 @@ if SERVER then
 				end
 			end
 		end
-		
+
 		if not IsValid(self.target) then self.target = self.Heavy.owner end
-	
+
 		local target = self.target
 		if IsValid(target) then
 			local heavy = self.Heavy
-			
+
 			for i=0, heavy:GetFlexNum() do
 				heavy:SetFlexWeight(i, math.random()*0.4)
 			end
-			
+
 			if math.random() > 0.999 then
 				self:PlaySound("vo/heavy_positivevocalization0"..math.random(5)..".mp3")
 			end
-		
+
 			local head = heavy:GetPhysicsObjectNum(14)
 			local lefthand = heavy:GetPhysicsObjectNum(11)
 			local righthand = heavy:GetPhysicsObjectNum(13)
 			local rightfoot = heavy:GetPhysicsObjectNum(15)
 			local leftfoot = heavy:GetPhysicsObjectNum(5)
 			local pelvis = heavy:GetPhysicsObjectNum(0)
-			
+
 			local velocity = (target:IsPlayer() and target:GetShootPos() or target:GetPos()) - heavy:GetPos()
-			
+
 			if target:IsPlayer() and target:GetShootPos():Distance(heavy:GetPos()) < 200 then
 				velocity = Vector()
-				
+
 				constraint.RemoveAll(self.Heavy)
 			end
-			
+
 			if target:GetClass() == "fishing_mod_seagull" then velocity = velocity:GetNormalized() * 1000 end
-			
+
 			local gravity = Vector(0,0,-20)
-			
+
 			head:AddVelocity(velocity)
 			lefthand:AddVelocity(velocity)
 			righthand:AddVelocity(velocity)
-			
+
 			head:AddAngleVelocity(Vector(-100,0,0))
 			-- leftfoot:AddVelocity(gravity)
-			
+
 			for i=0,15 do
 				local phys = heavy:GetPhysicsObject()
 				phys:EnableGravity(false)
 				local phys = heavy:GetPhysicsObjectNum(i)
-				phys:EnableGravity(false)	
+				phys:EnableGravity(false)
 				if self.target:GetClass() ~= "fishing_mod_seagull" then phys:AddVelocity(phys:GetVelocity()*-0.1) end
 			end
-			
+
 			--rightfoot:AddVelocity(gravity)
 			--leftfoot:AddVelocity(gravity)
-		end		
+		end
 		self:NextThink(CurTime())
 		return true
 	end
-	
+
 	function ENT:OnRemove()
 		if IsValid(self.Heavy) then self.Heavy:Remove() end
 	end
@@ -208,17 +208,17 @@ else
 		"bip_foot_r",
 		"bip_foot_l",
 	}
-	
+
 	function ENT:Think()
 		if self.dt.dead then return end
-		
+
 		local heavy = self.dt.Heavy
-		
+
 		for key, bone in pairs(bones) do
 			local bone = heavy:LookupBone(bone)
 			if bone then -- ???
 				local position = heavy:GetBonePosition(bone)
-				
+
 				local particle = self.emitter:Add( "effects/yellowflare", position )
 				particle:SetVelocity( VectorRand() * 10 )
 				particle:SetDieTime( 5 )
@@ -241,27 +241,27 @@ if SERVER and false then
 
 	local me = nero.GetPlayer("Caps")
 	local hitpos = me:GetEyeTrace().HitPos + Vector(0,0,200)
-	
+
 	if IsValid(heavy) then heavy:Remove() end
 	heavy = ents.Create("fishing_mod_catch_heavy")
 	heavy:SetPos(hitpos)
 	heavy:Spawn()
 	heavy:PostHook(me)
-	
+
 	local random = VectorRand()*1000
 	random.z = math.abs(random.z)
-	
+
 	if IsValid(sandwich) then sandwich:Remove() end
 	sandwich = ents.Create("prop_physics")
-	sandwich:SetModel("models/weapons/c_models/c_sandwich/c_sandwich.mdl")
+	sandwich:SetModel("models/food/burger.mdl")
 	sandwich:SetPos(hitpos+random)
 	sandwich:Spawn()
-		
+
 	if IsValid(seagull) then seagull:Remove() end
 	seagull = ents.Create("fishing_mod_seagull")
 	seagull:SetTargetOwner(me)
 	seagull:SetTarget(sandwich)
 	seagull:SetPos(hitpos+random)
 	seagull:Spawn()
-	
+
 end
